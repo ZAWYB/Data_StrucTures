f9a8f4c4ecf3841cbd18d5f225eff4d804c425d6
ZAWYB <2869789640@qq.com>
2025-07-17 16:34:34 +0800

    11

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*
+ *Data Structure Study
+ *https://www.bilibili.com/video/BV1Fv4y1f7T1/?spm_id_from=333.1387.homepage.video_card.click&vd_source=da06173a72d1931f0f4e3ebc6dc82a1f
+
+ *对于数据结构的讨论有两种方式：
+ *一是：数学和逻辑模型---抽象数据类型（ADT）
+ *二是：将数据结构作为具体实现来研究
+ *
+ *对于数据结构的研究：
+ *1.研究数据结构的逻辑视图
+ *2.研究数据结构可以提供的操作
+ *3.操作的成本(时间）
+ *4.用编程实现具体的操作
+ */
+
+
+
+
+//link list
+
